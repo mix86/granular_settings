1cd0993beeaf50bef6546449284a6f56974e4dc3
Mikhail Petrov <mixael@yandex-team.ru>
2011-09-07 20:56:29 +0400

    First bugfix

--- src/granular_settings/settings.py
+++ src/granular_settings/settings.py
@@ -4,7 +4,9 @@ import traceback
 
 settings_module_path = os.path.abspath(traceback.extract_stack()[-2][0])
 settings_dir_path = os.path.splitext(settings_module_path)[0]
-PROJECT_PATH = os.path.split(settings_module_path)[0]
+
+project_path = os.path.abspath(traceback.extract_stack()[0][0])
+PROJECT_PATH = os.path.split(project_path)[0]
 PROJECT = os.path.split(PROJECT_PATH)[-1]
 
 cf = glob.glob(os.path.join(settings_dir_path, '*.conf'))
